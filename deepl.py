df9608fe3f8be60a38040232a7ff08fc4887018d
Egor Chekashov <e.chekashov@gmail.com>
2020-04-04 03:38:46 +0300

    First test

--- deepl.py
+++ deepl.py
@@ -1,2 +1,26 @@
 #!/usr/local/bin/python3
 # Copyright 2020 Egor Chekashov
+
+import logging
+import configparser
+import pyppeteer
+from aiogram import Bot, Dispatcher, executor, types
+
+# Read config
+config = configparser.ConfigParser()
+config.read('../../caf.ini.php')
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+# Initialize bot and dispatcher
+bot = Bot(token=config['API Settings']['caf_token'])
+dp = Dispatcher(bot)
+
+# Handlers
+@dp.message_handler()
+async def echo(message: types.Message):
+    await message.answer(message.text)
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
