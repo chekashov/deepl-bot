483eb0089f8c9eb684f671e4f9b318a2b17bdea1
Egor Chekashov <e.chekashov@gmail.com>
2020-04-08 21:58:01 +0300

    Version 0.4
    - Refactored functions and handlers
    - Added global settings management
    - Improved callback menus
    - Improved handling of user profiles
    - Public by default (release)
    - Minor bug fixes and linter improvements

--- deepl.py
+++ deepl.py
@@ -1,16 +1,16 @@
 #!/usr/local/bin/python3
 # Copyright 2020 Egor Chekashov
-# DeepL Telegram v0.3
+# DeepL Telegram v0.4
 
 import logging
 import configparser
 import time as t
-import random as rnd
 from pathlib import Path as p
 import asyncio
 import pyppeteer as pp
 from aiogram import Bot, Dispatcher, executor, types
 
+VERSION = '0.4'
 
 # Main config
 def ini(section, key):
@@ -51,50 +51,55 @@ LANG = {
     'en': 'English',
     # 'pt-br': 'Portuguese (Brazilian)'
 }
-PHRASES = (
-    "All right, let\'s see 🔎",
-    "Give me a second, please ⏱",
-    "Looking for better options 👀",
-    "Great, new challenge 💪",
-    "I\'ll be right back 🏃",
-    "I need to think about it 🤔"
-)
 
 
-# Configure logging
+# Config management
 logging.basicConfig(level=logging.INFO)
 
-def user_init(file, uid):
+def user_init(uid, lang='en'):
     """ Creates a new (Chat ID).ini file
     """
+    file = p(USER_DATA / f'{uid}.ini')
     file.touch()
     if uid != ADMIN:
-        config['MAIN'] = {'lang': 'en'}
+        config['MAIN'] = {'lang': lang}
     else:
-        config['MAIN'] = {'lang': 'en',
+        config['MAIN'] = {'lang': lang,
                           'forward': 'yes',
-                          'debug': 'yes',
-                          'public': 'no'}
+                          'debug': 'no',
+                          'public': 'yes'}
+    config['STAT'] = {'version': VERSION,
+                      'total': '0'}
     with open(file, 'w') as settings:
         config.write(settings)
     config.clear()
 
-def set_conf(uid, key, value):
+def set_conf(uid, key, value, sect='MAIN'):
     """ Adds or updates the config key
     """
     file = p(USER_DATA / f'{uid}.ini')
     config.read(file)
-    config.set('MAIN', str(key), str(value))
+    config.set(sect, str(key), str(value))
     with open(file, 'w') as settings:
         config.write(settings)
     config.clear()
 
-def get_conf(uid, key):
+def inc_stat(uid, key):
+    """ Increment the config value
+    """
+    file = p(USER_DATA / f'{uid}.ini')
+    config.read(file)
+    config.set('STAT', str(key), str(int(ini('STAT', key)) + 1))
+    with open(file, 'w') as settings:
+        config.write(settings)
+    config.clear()
+
+def get_conf(uid, key, sect='MAIN'):
     """ Returns the config value
     """
     file = p(USER_DATA / f'{uid}.ini')
     config.read(file)
-    value = ini('MAIN', key)
+    value = ini(sect, key)
     config.clear()
     return value
 
@@ -108,9 +113,42 @@ def del_conf(uid, key):
         config.write(settings)
     config.clear()
 
+def check_ver(uid):
+    try:
+        ver = get_conf(uid, 'version', 'STAT')
+    except KeyError:
+        ver = None
+    try:
+        lang = get_conf(uid, 'lang')
+    except KeyError:
+        lang = 'en'
+    if ver != VERSION:
+        p(USER_DATA / f'{uid}.ini').unlink(missing_ok=True)
+        user_init(uid, lang)
+
+
+# Core functions
 def get_button(conf, txt):
     icon = '🔳 ' if str_to_bool(get_conf(ADMIN, conf)) else '⬜️ '
     return icon + txt
+
+def update_settings():
+    global SETTINGS
+    SETTINGS = {
+        'forward': [
+            get_conf(ADMIN, 'forward'),
+            get_button('forward', 'Forward')
+        ],
+        'debug': [
+            get_conf(ADMIN, 'debug'),
+            get_button('debug', 'Debug')
+        ],
+        'public': [
+            get_conf(ADMIN, 'public'),
+            get_button('public', 'Public')
+        ]
+    }
+    return SETTINGS
 
 def sec_to_time(sec):
     """ Returns the formatted time H:MM:SS
@@ -125,12 +163,30 @@ def sec_to_time(sec):
     ftime += f"{s:02d}" if h and m else f"{s:.2f} sec"
     return ftime
 
+def collect_buttons(btn_dict, close=False, rw=ROW_WIDTH):
+    buttons = types.InlineKeyboardMarkup(row_width=rw)
+    if btn_dict is SETTINGS:
+        btn_dict = update_settings()
+    if close:
+        btn_dict['close'] = '❌ Close'
+    for r in range(0, len(btn_dict), rw):
+        args = ""
+        for k in dict(list(btn_dict.items())[0+r:rw+r]):
+            if isinstance(btn_dict[k], str):
+                args += f'in_btn("{btn_dict[k]}", '
+            else:
+                args += f'in_btn("{btn_dict[k][1]}", '
+            args += f'callback_data="{k}"), '
+        args = args.rstrip(', ')
+        exec(f"buttons.row({args})", globals(), locals())
+    return buttons
+
 
 # Wrappers
 def debug(msg, uid):
     """ Returns a debugging information if enabled
     """
-    if DEBUG and (uid == ADMIN or uid in TESTERS):
+    if get_glob('debug') and (uid == ADMIN or uid in TESTERS):
         return '🛠 ' + msg + N
     return ""
 
@@ -144,7 +200,19 @@ def filter_output(txt):
     return txt
 
 def str_to_bool(txt):
-    return str(txt.lower()) in ("true", "yes", "on", "1")
+    return str(txt).lower() in ("true", "yes", "on", "1")
+
+def get_glob(key):
+    return str_to_bool(SETTINGS[key][0])
+
+def set_glob(key, value):
+    global SETTINGS
+    if str(value).lower() in ("true", "yes", "on", "1"):
+        SETTINGS[key][0] = 'yes'
+        set_conf(ADMIN, key, 'yes')
+    else:
+        SETTINGS[key][0] = 'no'
+        set_conf(ADMIN, key, 'no')
 
 
 # Extract via pyppeteer
@@ -171,10 +239,12 @@ async def translate(uid, txt):
     # Execute
     await page.goto(f"{BASE_URL}#*/{lang}/{txt}")
     try:
-        await page.waitForFunction(TRG_JS + '.length > 0', timeout=2000)
+        TRG_LEN = TRG_JS + '.length > 0'
+        await page.waitForFunction(TRG_LEN, timeout=10000)
     except pp.errors.TimeoutError:
         pass
     result = await page.evaluate(TRG_JS)
+    # await page.screenshot({'path': 'before_close.png'})
     await page.close()
     await browser.disconnect()
     return result
@@ -190,18 +260,17 @@ async def start(message: types.Message):
     msg = ""
     if uid != ADMIN:
         await message.forward(ADMIN, message.text)
-        if not PUBLIC and uid not in TESTERS:
+        if not get_glob('public') and uid not in TESTERS:
             return
-    file = p(USER_DATA / f'{uid}.ini')
     # If a new user, create an ini file
-    if not file.exists():
-        user_init(file, uid)
+    if not p(USER_DATA / f'{uid}.ini').exists():
+        user_init(uid)
         msg += debug("New user, file was created" + N, uid)
         msg += f"Nice to meet you, {message.chat.first_name}. "
     else:
+        check_ver(uid)
         msg += debug(f"User file exists <b>{uid}.ini</b>" + N, uid)
         msg += f"Welcome back, {message.chat.first_name}. "
-    del file
     # Join and send a message
     msg += "Just send me a message and I'll translate it into English. "
     msg += "You can change the translation /language at any time."
@@ -214,58 +283,83 @@ async def language(message: types.Message):
     """
     # Required block
     uid = message.chat.id
+    check_ver(uid)
+    msg = ""
     if uid != ADMIN:
         await message.forward(ADMIN, message.text)
-        if not PUBLIC and uid not in TESTERS:
+        if not get_glob('public') and uid not in TESTERS:
             return
-    # Collect buttons for the inline keyboard
-    inline_kb = types.InlineKeyboardMarkup(row_width=ROW_WIDTH)
-    for r in range(0, len(LANG), ROW_WIDTH):
-        args = ""
-        for key in dict(list(LANG.items())[0+r:ROW_WIDTH+r]):
-            args += f'in_btn("{LANG[key]}", callback_data="{key}"), '
-        args = args.rstrip(', ')
-        exec(f"inline_kb.row({args})", globals(), locals())
     # Send inline keyboard
-    await message.answer("Choose a translation language",
-                         reply_markup=inline_kb)
+    inline_kb = collect_buttons(LANG, close=True)
+    msg += f"I'm translating into <b>{LANG[get_conf(uid, 'lang')]}</b>"
+    msg += ", but I also know other languages"
+    await message.answer(msg, reply_markup=inline_kb, parse_mode='html')
+
+
+@dp.callback_query_handler(lambda cb: cb.data == 'close')
+async def callback_close(query: types.CallbackQuery):
+    """ Handles callback after changing settings via admin menu
+    """
+    # Required block
+    uid = query.message.chat.id
+    check_ver(uid)
+    msg_id = query.message.message_id
+    btn = query.data
+    await query.answer("Cleaning up ✨")
+    await bot.delete_message(uid, msg_id)
 
 
 @dp.callback_query_handler(lambda cb: cb.data in LANG.keys())
-async def inline_kb_answer(query: types.CallbackQuery):
+async def callback_lang(query: types.CallbackQuery):
     """ Handles callback after language selection
     """
-    await query.answer("Good choice 👌")
+    # Required block
     uid = query.message.chat.id
+    check_ver(uid)
     msg_id = query.message.message_id
     btn = query.data
+    # Apply new settings
     msg = ""
     msg += debug("uid = " + str(uid), uid)
     msg += debug("msg_id = " + str(msg_id), uid)
     msg += debug("btn = " + str(btn) + N, uid)
-    msg += f"Now I'll be translating into {LANG[btn]}"
+    msg += f"Now I'll be translating into <b>{LANG[btn]}</b>"
     set_conf(uid, 'lang', btn)
-    await bot.edit_message_text(msg, uid, msg_id)
+    await query.answer("Good choice 👌")
+    await bot.edit_message_text(msg, uid, msg_id, parse_mode='html')
 
 
-@dp.message_handler(commands=['admin'])
+@dp.callback_query_handler(lambda cb: cb.data in SETTINGS.keys())
+async def callback_admin(query: types.CallbackQuery):
+    """ Handles callback after changing settings via admin menu
+    """
+    # Required block
+    uid = query.message.chat.id
+    check_ver(uid)
+    msg_id = query.message.message_id
+    btn = query.data
+    # Apply new settings
+    new_val = 0 if get_glob(btn) else 1
+    msg = query.message.text
+    set_glob(btn, new_val)
+    await query.answer("Settings saved 👌")
+    inline_kb = collect_buttons(SETTINGS, close=True)
+    await bot.edit_message_text(msg, ADMIN, msg_id,
+        reply_markup=inline_kb)
+
+
+@dp.message_handler(commands=['a'])
 async def admin_menu(message: types.Message):
     """ Sends an interactive message with admin settings
     """
     # Required block
     uid = message.chat.id
+    check_ver(uid)
     if uid != ADMIN:
         await message.forward(ADMIN, message.text)
         return
-    # Collect buttons for the inline keyboard
-    inline_kb = types.InlineKeyboardMarkup(row_width=ROW_WIDTH)
-    for r in range(0, len(SETTINGS), ROW_WIDTH):
-        args = ""
-        for key in dict(list(SETTINGS.items())[0+r:ROW_WIDTH+r]):
-            args += f'in_btn("{SETTINGS[key][1]}", callback_data="{key}"), '
-        args = args.rstrip(', ')
-        exec(f"inline_kb.row({args})", globals(), locals())
     # Send inline keyboard
+    inline_kb = collect_buttons(SETTINGS, close=True)
     await message.answer("Welcome back, Commander",
                          reply_markup=inline_kb)
 
@@ -276,50 +370,35 @@ async def echo_result(message: types.Message):
     """
     # Required block
     uid = message.chat.id
+    check_ver(uid)
     msg = ""
     if uid != ADMIN:
         await message.forward(ADMIN, message.text)
-        if not PUBLIC and uid not in TESTERS:
+        if not get_glob('public') and uid not in TESTERS:
             return
-    sent = await message.answer(rnd.choice(PHRASES))
+    sent = await message.answer('📝')
     t_start = t.time()
     result = await translate(uid, message.text)
     t_diff = (t.time() - t_start)
-    msg += debug(f"Translation took {sec_to_time(t_diff)}" + N, uid)
-    msg += filter_output(result)
     if filter_output(result):
+        msg += debug(f"Translation took {sec_to_time(t_diff)}" + N, uid)
+        msg += filter_output(result)
         sent = await bot.edit_message_text(msg, uid, sent.message_id)
         if uid != ADMIN:
             await bot.forward_message(ADMIN, sent.chat.id,
                 sent.message_id)
     else:
         await bot.delete_message(sent.chat.id, sent.message_id)
+    inc_stat(uid, 'total')
 
 
 if __name__ == '__main__':
     if not USER_DATA.exists():
         print("There's no directory for storing settings, creating...")
         USER_DATA.mkdir()
-    file = p(USER_DATA / f'{ADMIN}.ini')
-    if not file.exists():
+    if not p(USER_DATA / f'{ADMIN}.ini').exists():
         print("There's no admin defaults, creating...")
-        user_init(file, ADMIN)
-    del file
-    SETTINGS = {
-        'forward':  [
-                        get_conf(ADMIN, 'forward'),
-                        get_button('forward', 'Forward')
-                    ],
-        'debug':    [
-                        get_conf(ADMIN, 'debug'),
-                        get_button('debug', 'Debug')
-                    ],
-        'public':   [
-                        get_conf(ADMIN, 'public'),
-                        get_button('public', 'Public')
-                    ]
-    }
-    FORWARD = str_to_bool(SETTINGS['forward'][0])
-    DEBUG = str_to_bool(SETTINGS['debug'][0])
-    PUBLIC = False
+        user_init(ADMIN)
+    SETTINGS = {}
+    update_settings()
     executor.start_polling(dp, skip_updates=True)
